e6bd0fe625868cf3bd99ddd51e02ebd139d5d529
Asha G R <115894855+ashagr07@users.noreply.github.com>
2022-10-15 23:51:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blurberry Oatmeal')
+streamlit.text('Kale and Spinach smoothie')
+streamlit.text('Boiled Egg')
